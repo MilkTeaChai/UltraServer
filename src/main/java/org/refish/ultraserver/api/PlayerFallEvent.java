44be7d3e4233d59392cdc98642dc3600121e8dbc
MilkTeaChai <teacherchai@foxmail.com>
2023-01-23 21:08:05 +0800

    使算法更节能，不耗费CPU计算资源

--- src/main/java/org/refish/ultraserver/api/PlayerFallEvent.java
+++ src/main/java/org/refish/ultraserver/api/PlayerFallEvent.java
@@ -8,14 +8,16 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 
-public class PlayerFallEvent extends PlayerEvent {
+public class PlayerFallEvent extends PlayerEvent implements Listener {
     private static final HandlerList handler = new HandlerList();
+    World world;
 
     public PlayerFallEvent(@NotNull Player player) {
         super(player);
@@ -33,14 +35,17 @@ public class PlayerFallEvent extends PlayerEvent {
         int BlockLocationY = player.getLocation().getBlock().getY();
         int BlockLocationZ = player.getLocation().getBlock().getZ();
         int BlockLocationX = player.getLocation().getBlock().getX();
-        int newestBlockLocationY=BlockLocationY;
-        Block block= Objects.requireNonNull(Bukkit.getWorld("world")).getBlockAt(BlockLocationX,newestBlockLocationY,BlockLocationZ);
-        do{
-            newestBlockLocationY=newestBlockLocationY-1;
-        }while(Objects.equals(block.getType(), Material.AIR));
-        if(newestBlockLocationY>4){
-            Event PlayerFallEvent = new PlayerFallEvent(event.getPlayer());
-            Bukkit.getServer().getPluginManager().callEvent(PlayerFallEvent);
+        //预检查是否进入伪摔落状态
+        if(Objects.requireNonNull(world).getBlockAt(BlockLocationX,BlockLocationY,BlockLocationZ).getType()==Material.AIR){
+            int newestBlockLocationY=BlockLocationY;
+            Block block= Objects.requireNonNull(world).getBlockAt(BlockLocationX,newestBlockLocationY,BlockLocationZ);
+            do{
+                newestBlockLocationY=newestBlockLocationY-1;
+            }while(Objects.equals(block.getType(), Material.AIR));
+            if(newestBlockLocationY>4){
+                Event PlayerFallEvent = new PlayerFallEvent(event.getPlayer());
+                Bukkit.getServer().getPluginManager().callEvent(PlayerFallEvent);
+            }
         }
     }
     @NotNull
@@ -51,5 +56,9 @@ public class PlayerFallEvent extends PlayerEvent {
 
     public static HandlerList getHandlerList() {
         return handler;
+    }
+
+    public void getWorld(World world){
+        this.world=world;
     }
 }
